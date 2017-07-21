ad74fddcac1276d885a9da1a6b92fd707b8092f7
Xingjian Xu <dotswing@gmail.com>
2017-07-21 21:17:20 +0800

    Move contents in app_init.rb to seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+unless User.find_by_role :admin
+  admin_user = User.find_or_create_by(email: Config.app.admin.email)
+  admin_user.password = '12345678'
+  admin_user.password_confirmation = '12345678'
+  admin_user.role = :admin
+  admin_user.save
+end
+
+Article.create_homepage! unless Article.homepage
